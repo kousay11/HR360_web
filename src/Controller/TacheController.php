f172cb65177a339983317fa8adab27f358fba18b
ChkiliSaif741 <chekili.saih@esprit.tn>
2025-04-04 00:15:15 +0100

    projet --> taches

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -10,22 +10,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Projet;
 
 #[Route('/tache')]
 final class TacheController extends AbstractController
 {
-    #[Route(name: 'app_tache_index', methods: ['GET'])]
-    public function index(TacheRepository $tacheRepository): Response
+    #[Route('/', name: 'app_tache_index', methods: ['GET'])]
+    #[Route('/project/{id}', name: 'app_tache_by_project', methods: ['GET'])]
+    public function index(?Projet $projet, TacheRepository $tacheRepository): Response
     {
+        $taches = $projet 
+            ? $tacheRepository->findBy(['projet' => $projet])
+            : $tacheRepository->findAll();
+
         return $this->render('tache/index.html.twig', [
-            'taches' => $tacheRepository->findAll(),
+            'taches' => $taches,
+            'projet' => $projet
         ]);
     }
 
-    #[Route('/new', name: 'app_tache_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/new/{projetId}', name: 'app_tache_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, ?int $projetId = null): Response
     {
         $tache = new Tache();
+        
+        // If projetId is provided, set it on the new task
+        if ($projetId) {
+            $projet = $entityManager->getRepository(Projet::class)->find($projetId);
+            if ($projet) {
+                $tache->setProjet($projet);
+            }
+        }
+
         $form = $this->createForm(TacheType::class, $tache);
         $form->handleRequest($request);
 
@@ -33,49 +49,78 @@ final class TacheController extends AbstractController
             $entityManager->persist($tache);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_tache_index', [], Response::HTTP_SEE_OTHER);
+            // Redirect back to project's tasks if coming from project
+            $redirectRoute = $projetId 
+                ? 'app_tache_by_project' 
+                : 'app_tache_index';
+                
+            return $this->redirectToRoute($redirectRoute, [
+                'id' => $projetId
+            ], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('tache/new.html.twig', [
             'tache' => $tache,
             'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_tache_show', methods: ['GET'])]
-    public function show(Tache $tache): Response
-    {
-        return $this->render('tache/show.html.twig', [
-            'tache' => $tache,
+            'projetId' => $projetId
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_tache_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Tache $tache, EntityManagerInterface $entityManager): Response
     {
+        $projetId = $tache->getProjet() ? $tache->getProjet()->getId() : null;
+        
         $form = $this->createForm(TacheType::class, $tache);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_tache_index', [], Response::HTTP_SEE_OTHER);
+            // Redirect back to project's tasks if task belongs to project
+            $redirectRoute = $projetId 
+                ? 'app_tache_by_project' 
+                : 'app_tache_index';
+                
+            return $this->redirectToRoute($redirectRoute, [
+                'id' => $projetId
+            ], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('tache/edit.html.twig', [
             'tache' => $tache,
             'form' => $form,
+            'projetId' => $projetId
         ]);
     }
 
     #[Route('/{id}', name: 'app_tache_delete', methods: ['POST'])]
     public function delete(Request $request, Tache $tache, EntityManagerInterface $entityManager): Response
     {
+        $projetId = $tache->getProjet() ? $tache->getProjet()->getId() : null;
+        
         if ($this->isCsrfTokenValid('delete'.$tache->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($tache);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_tache_index', [], Response::HTTP_SEE_OTHER);
+        // Redirect back to project's tasks if task belonged to project
+        $redirectRoute = $projetId 
+            ? 'app_tache_by_project' 
+            : 'app_tache_index';
+            
+        return $this->redirectToRoute($redirectRoute, [
+            'id' => $projetId
+        ], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}', name: 'app_tache_show', methods: ['GET'])]
+    public function show(Tache $tache): Response
+    {
+        $projetId = $tache->getProjet() ? $tache->getProjet()->getId() : null;
+        return $this->render('tache/show.html.twig', [
+            'tache' => $tache,
+            'projetId' => $projetId
+        ]);
     }
 }
--- src/Repository/TacheRepository.php
+++ src/Repository/TacheRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Tache;
+use App\Entity\Projet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -12,6 +13,14 @@ class TacheRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Tache::class);
     }
-
+    // src/Repository/TacheRepository.php
+public function findByProject(Projet $projet)
+{
+    return $this->createQueryBuilder('t')
+        ->andWhere('t.projet = :projet')
+        ->setParameter('projet', $projet)
+        ->getQuery()
+        ->getResult();
+}
     // Add custom methods as needed
 }
