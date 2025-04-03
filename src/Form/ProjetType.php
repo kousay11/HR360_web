d2da55a9b455039503cb0dfb8d83d31615799977
ChkiliSaif741 <chekili.saih@esprit.tn>
2025-04-03 22:57:18 +0100

    projet crud avec css

--- src/Form/ProjetType.php
+++ src/Form/ProjetType.php
@@ -6,23 +6,45 @@ use App\Entity\Projet;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ProjetType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('nom')
-            ->add('description')
-            ->add('dateDebut', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('dateFin', null, [
-                'widget' => 'single_text',
-            ])
-        ;
-    }
-
+    // src/Form/ProjetType.php
+public function buildForm(FormBuilderInterface $builder, array $options): void
+{
+    $builder
+        ->add('nom', null, [
+            'label' => 'Project Name',
+            'attr' => [
+                'placeholder' => 'Enter project name'
+            ]
+        ])
+        ->add('description', null, [
+            'label' => 'Description',
+            'attr' => [
+                'placeholder' => 'Describe the project details',
+                'rows' => 4
+            ]
+        ])
+        ->add('dateDebut', DateType::class, [
+            'label' => 'Start Date',
+            'widget' => 'single_text',
+            'html5' => true,
+            'attr' => [
+                'class' => 'datepicker'
+            ]
+        ])
+        ->add('dateFin', DateType::class, [
+            'label' => 'End Date',
+            'widget' => 'single_text',
+            'html5' => true,
+            'attr' => [
+                'class' => 'datepicker'
+            ]
+        ])
+    ;
+}
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
