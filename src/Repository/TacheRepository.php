850ad9b8cccf52cce9bee4ca32680ec4366f67ae
ChkiliSaif741 <chekili.saih@esprit.tn>
2025-04-27 10:54:52 +0100

    Filtre par statut tache back

--- src/Repository/TacheRepository.php
+++ src/Repository/TacheRepository.php
@@ -7,6 +7,7 @@ use App\Entity\Projet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Equipe;
+
 class TacheRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -14,72 +15,82 @@ class TacheRepository extends ServiceEntityRepository
         parent::__construct($registry, Tache::class);
     }
     // src/Repository/TacheRepository.php
-public function findByProject(Projet $projet)
-{
-    return $this->createQueryBuilder('t')
-        ->andWhere('t.projet = :projet')
-        ->setParameter('projet', $projet)
-        ->getQuery()
-        ->getResult();
-}
-public function search(string $query): array
-{
-    return $this->createQueryBuilder('t')
-        ->where('t.nom LIKE :query')
-        ->setParameter('query', '%'.$query.'%')
-        ->getQuery()
-        ->getResult();
-}
-
-public function searchByProject(Projet $projet, string $query): array
-{
-    return $this->createQueryBuilder('t')
-        ->where('t.projet = :projet')
-        ->andWhere('(t.nom LIKE :query)')
-        ->setParameter('projet', $projet)
-        ->setParameter('query', '%'.$query.'%')
-        ->getQuery()
-        ->getResult();
-}
-public function prioritize(): array
-{
-    return $this->createQueryBuilder('t')
-        ->orderBy('t.dateFin', 'ASC')
-        ->getQuery()
-        ->getResult();
-}
-
-public function prioritizeByProject(Projet $projet): array
-{
-    return $this->createQueryBuilder('t')
-        ->where('t.projet = :projet')
-        ->setParameter('projet', $projet)
-        ->orderBy('t.dateFin', 'ASC')
-        ->getQuery()
-        ->getResult();
-}
-public function findAllForExport(?Projet $projet = null): array
-{
-    $qb = $this->createQueryBuilder('t')
-        ->orderBy('t.statut', 'ASC')
-        ->addOrderBy('t.dateFin', 'ASC');
-
-    if ($projet) {
-        $qb->andWhere('t.projet = :projet')
-           ->setParameter('projet', $projet);
+    public function findByProject(Projet $projet)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.projet = :projet')
+            ->setParameter('projet', $projet)
+            ->getQuery()
+            ->getResult();
+    }
+    public function search(string $query): array
+    {
+        return $this->createQueryBuilder('t')
+            ->where('t.nom LIKE :query')
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
     }
 
-    return $qb->getQuery()->getResult();
-}
-public function findTasksOfTeamWithTrello(Equipe $equipe): array
-{
-    return $this->createQueryBuilder('t')
-        ->innerJoin('t.projet', 'p')
-        ->innerJoin('p.projetequipes', 'pe')
-        ->where('pe.equipe = :equipe')
-        ->andWhere('t.trelloboardid IS NOT NULL')
-        ->setParameter('equipe', $equipe)
-        ->getQuery()
-        ->getResult();
-}
+    public function searchByProject(Projet $projet, string $query): array
+    {
+        return $this->createQueryBuilder('t')
+            ->where('t.projet = :projet')
+            ->andWhere('(t.nom LIKE :query)')
+            ->setParameter('projet', $projet)
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
+    }
+    public function prioritize(): array
+    {
+        return $this->createQueryBuilder('t')
+            ->orderBy('t.dateFin', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function prioritizeByProject(Projet $projet): array
+    {
+        return $this->createQueryBuilder('t')
+            ->where('t.projet = :projet')
+            ->setParameter('projet', $projet)
+            ->orderBy('t.dateFin', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+    public function findAllForExport(?Projet $projet = null): array
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->orderBy('t.statut', 'ASC')
+            ->addOrderBy('t.dateFin', 'ASC');
+
+        if ($projet) {
+            $qb->andWhere('t.projet = :projet')
+                ->setParameter('projet', $projet);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+    public function findTasksOfTeamWithTrello(Equipe $equipe): array
+    {
+        return $this->createQueryBuilder('t')
+            ->innerJoin('t.projet', 'p')
+            ->innerJoin('p.projetequipes', 'pe')
+            ->where('pe.equipe = :equipe')
+            ->andWhere('t.trelloboardid IS NOT NULL')
+            ->setParameter('equipe', $equipe)
+            ->getQuery()
+            ->getResult();
+    }
+    public function findByStatusAndProject(Projet $projet, string $status): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.projet = :projet')
+            ->andWhere('t.statut = :status')
+            ->setParameter('projet', $projet)
+            ->setParameter('status', $status)
+            ->getQuery()
+            ->getResult();
+    }
 }
